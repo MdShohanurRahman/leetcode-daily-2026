40a88b681fc4f2b2a184050ee9c52dfc112e4791
shohan <shohan.drmc@gmail.com>
2026-01-03 12:06:16 +0800

    LC_961.java

---
+++ src/leecode/LC_961.java
@@ -0,0 +1,22 @@
+package leecode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LC_961 {
+    public int repeatedNTimes(int[] nums) {
+        // Create a HashSet to store unique elements
+        // Initial capacity is set to n/2 + 1 for optimization
+        Set<Integer> seenNumbers = new HashSet<>(nums.length / 2 + 1);
+
+        // Iterate through the array
+        for (int i = 0; ; ++i) {
+            // Try to add current element to the set
+            // add() returns false if element already exists
+            if (!seenNumbers.add(nums[i])) {
+                // Found the duplicate element that appears n times
+                return nums[i];
+            }
+        }
+    }
+}
